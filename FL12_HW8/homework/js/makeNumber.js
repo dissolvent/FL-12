f7bea1e1a7e92b8702f37cf433bac0e27caadf31
Dissolvent <psykhinin@gmail.com>
2019-12-30 17:06:34 +0200

    Update makeNumber.js

--- FL12_HW8/homework/js/makeNumber.js
+++ FL12_HW8/homework/js/makeNumber.js
@@ -1,9 +1,11 @@
 function makeNumber(inputString) {
     let output = '';
+
     for (let char of inputString) {
         if (!isNaN(char)) {
             output += char;
         }
     }
+    
     return output;
 }
