eabc800834690ae86eba7363bc3eba8b4e799a0a
Dissolvent <psykhinin@gmail.com>
2019-12-30 14:23:45 +0200

    Create makeNumber.js

---
+++ FL12_HW8/homework/js/makeNumber.js
@@ -0,0 +1,9 @@
+function makeNumber(inputString) {
+    let output = '';
+    for (let char of inputString) {
+        if (!isNaN(char)) {
+            output += char;
+        }
+    }
+    return output;
+}
